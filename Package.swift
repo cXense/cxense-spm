1db66d74a5b3e389545683e86132c9146305ca10
Alexey Troshkov <55428673+alexey-troshkov@users.noreply.github.com>
2024-12-10 17:59:44 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -37,12 +37,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "CxenseSDK",
-            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-iOS-1.10.0.zip",
+            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-iOS-1.10.1.zip",
             checksum: "a41788306fefd9ee372ab6a1ff0eed08a539aa74f68f9205c93fb0f90d059a44"
         ),
         .binaryTarget(
             name: "CxenseSDKTv",
-            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-tvOS-1.10.0.zip",
+            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-tvOS-1.10.1.zip",
             checksum: "5d200bde8d74c493bb8e356d26fab68f36039f5e94956c06f2eb04a5ae87ba2b"
         )
     ]
