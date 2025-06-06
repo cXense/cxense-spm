a463931f906f2c906955998c591f3b071fd607da
Alexey Troshkov <alexey.troshkov@piano.io>
2025-06-06 08:38:56 +0300

    v1.10.2

--- Package.swift
+++ Package.swift
@@ -37,13 +37,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "CxenseSDK",
-            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-iOS-1.10.1.zip",
-            checksum: "a41788306fefd9ee372ab6a1ff0eed08a539aa74f68f9205c93fb0f90d059a44"
+            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-iOS-1.10.2.zip",
+            checksum: "ab535b713d53a60165d835a122c3fefe87c2cf1ba225da513e54b0ec409347ee"
         ),
         .binaryTarget(
             name: "CxenseSDKTv",
-            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-tvOS-1.10.1.zip",
-            checksum: "5d200bde8d74c493bb8e356d26fab68f36039f5e94956c06f2eb04a5ae87ba2b"
+            url: "https://s3.amazonaws.com/sdk.cxense.com/CxenseSDK-tvOS-1.10.2.zip",
+            checksum: "71ae3e051abcfcd3096b60c0c2def790b05be12cdfd46138da318cb058053150"
         )
     ]
 )
